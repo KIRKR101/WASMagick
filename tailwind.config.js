1ddb21ff96d501f6e6723b113b1916c6f165ba13
KIRKR101 <104659112+KIRKR101@users.noreply.github.com>
2025-08-01 13:13:59 +0100

    initial

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./script.js",
+  ],
+  theme: {
+    fontFamily: {
+      sans: ['Google Sans Code', 'monospace'],
+      mono: ['Google Sans Code', 'monospace'],
+      'google-sans-code': ['Google Sans Code', 'monospace'],
+    },
+  },
+  plugins: [],
+}
